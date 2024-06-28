b7a5e73e8cd8ab935791961e014b5ca7adad6b9c
Hikari Luz <hikaridesuyoo@gmail.com>
2024-06-28 17:48:00 -0300

    feat: add more spaceship controls + skybox

--- Assets/Game/Spaceship.cs
+++ Assets/Game/Spaceship.cs
@@ -8,10 +8,20 @@ public class Spaceship : UdonSharpBehaviour
 {
     // Defined by a script listening to the VRCStation events
     public bool localPlayerUsing = false;
+
     public float engineForce;
+    public float engineRotationalForce;
+
+    // How much force is used for stabilization
+    // 0 means 0% of engineForce
+    // 100 means 100% of engineForce
+    public float stabilization;
+
+    // How much force is used for stabilization
+    // of angular force. Same as stabilization
+    // but relative to engineRotationalForce
+    public float rotationalStabilization;
     private Rigidbody _rb;
-    // Defines the targeted roll of the spacecraft: https://en.wikipedia.org/wiki/Aircraft_principal_axes
-    private float _roll = 0f;
 
     void Start()
     {
@@ -24,47 +34,93 @@ public class Spaceship : UdonSharpBehaviour
     // engine force appearing stronger or weaker
     void FixedUpdate()
     {
-        // We only want to handle controls for the local player.
-        // Each client will deal with their own spaceship and
-        // then sync the transform
+        // We only want to handle spaceship movement for the
+        // local player. Each client will deal with their own
+        // spaceship and then sync the transform
         if(!localPlayerUsing) return;
 
-        HandleRotation();
+        HandleForce();
+        HandleTorque();
+    }
 
+    private void HandleForce()
+    {
         // When space key is pressed, the spaceship receives force upwards
         // (relative to the spaceship, hence the `transform.rotation *`) on
         // acceleration mode (continuous, mass-independent)
         if (Input.GetKey(KeyCode.Space))
         {
-            _rb.AddForce(transform.rotation * Vector3.up * engineForce, ForceMode.Acceleration);
+            ApplyForce(transform.rotation * Vector3.up * engineForce);
+            return;
         }
 
         // When shift key is pressed, the spaceship receives force forward
         // in a similar way to the space key behaviour.
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            _rb.AddForce(transform.rotation * Vector3.forward * engineForce, ForceMode.Acceleration);
+            ApplyForce(transform.rotation * Vector3.forward * engineForce);
+            return;
         }
 
-
-        // Roll is controlled by using Q and E keys
-        if (Input.GetKey(KeyCode.Q)) _roll += 5f;
-
-        if (Input.GetKey(KeyCode.E)) _roll -= 5f;
+        // Stabilize force
+        // when it is not being controlled by user
+        ApplyForce(-_rb.velocity.normalized * engineForce * stabilization);
     }
 
-    // Uses Slerp method to smoothly rotate the spaceship
-    // in the targeted rotation, defined by where the player
-    // is looking + the target roll angle
-    private void HandleRotation()
+    // Applies angular force to spin the ship based
+    // on user input
+    private void HandleTorque()
     {
-        var targetEulerRotation = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation.eulerAngles;
-        targetEulerRotation.z = _roll;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ApplyTorque(transform.rotation * Vector3.forward * engineRotationalForce);
+            return;
+        }
 
-        transform.rotation = Quaternion.Slerp(
-            transform.rotation,
-            Quaternion.Euler(targetEulerRotation),
-            Time.deltaTime
-        );
+        if (Input.GetKey(KeyCode.E))
+        {
+            ApplyTorque(transform.rotation * -Vector3.forward * engineRotationalForce);
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyTorque(transform.rotation * -Vector3.up * engineRotationalForce);
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            ApplyTorque(transform.rotation * Vector3.up * engineRotationalForce);
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            ApplyTorque(transform.rotation * -Vector3.right * engineRotationalForce);
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            ApplyTorque(transform.rotation * Vector3.right * engineRotationalForce);
+            return;
+        }
+
+        // Stabilize angular motion
+        // when it is not being controlled by user
+        ApplyTorque(-_rb.angularVelocity.normalized * engineRotationalForce * rotationalStabilization);
+    }
+
+    private void ApplyTorque(Vector3 torque)
+    {
+        // ForceMode.Force means continuous force, considering mass
+        _rb.AddTorque(torque, ForceMode.Force);
+    }
+
+    private void ApplyForce(Vector3 torque)
+    {
+        // ForceMode.Force means continuous force, considering mass
+        _rb.AddForce(torque, ForceMode.Force);
     }
 }
