8864ae688647ddc95fda5e689cce05fa2270c1ad
Hikari Luz <hikaridesuyoo@gmail.com>
2024-06-28 14:23:18 -0300

    feat: skybox + fix some controls on spaceship

--- Assets/Game/Spaceship.cs
+++ Assets/Game/Spaceship.cs
@@ -6,32 +6,65 @@ using VRC.Udon;
 
 public class Spaceship : UdonSharpBehaviour
 {
-    public VRCStation station;
-    public float upForce = 10f;
+    // Defined by a script listening to the VRCStation events
+    public bool localPlayerUsing = false;
+    public float engineForce;
     private Rigidbody _rb;
+    // Defines the targeted roll of the spacecraft: https://en.wikipedia.org/wiki/Aircraft_principal_axes
     private float _roll = 0f;
 
     void Start()
     {
+        // Caching components is good practice!
         _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    // FixedUpdate is called instead of Update to prevent
+    // different behaviours based on the players fps, e.g.
+    // engine force appearing stronger or weaker
+    void FixedUpdate()
     {
-        var rotation = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
-        Vector3 eulerRotation = rotation.eulerAngles;
-        eulerRotation.z = _roll;
-        rotation = Quaternion.Euler(eulerRotation);
+        // We only want to handle controls for the local player.
+        // Each client will deal with their own spaceship and
+        // then sync the transform
+        if(!localPlayerUsing) return;
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        HandleRotation();
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        // When space key is pressed, the spaceship receives force upwards
+        // (relative to the spaceship, hence the `transform.rotation *`) on
+        // acceleration mode (continuous, mass-independent)
+        if (Input.GetKey(KeyCode.Space))
         {
-            _rb.AddForce(transform.rotation * Vector3.up * upForce, ForceMode.Impulse);
+            _rb.AddForce(transform.rotation * Vector3.up * engineForce, ForceMode.Acceleration);
         }
 
-        if (Input.GetKeyUp(KeyCode.Q)) _roll -= 5f;
+        // When shift key is pressed, the spaceship receives force forward
+        // in a similar way to the space key behaviour.
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _rb.AddForce(transform.rotation * Vector3.forward * engineForce, ForceMode.Acceleration);
+        }
 
-        if (Input.GetKeyUp(KeyCode.E)) _roll += 5f;
+
+        // Roll is controlled by using Q and E keys
+        if (Input.GetKey(KeyCode.Q)) _roll += 5f;
+
+        if (Input.GetKey(KeyCode.E)) _roll -= 5f;
+    }
+
+    // Uses Slerp method to smoothly rotate the spaceship
+    // in the targeted rotation, defined by where the player
+    // is looking + the target roll angle
+    private void HandleRotation()
+    {
+        var targetEulerRotation = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation.eulerAngles;
+        targetEulerRotation.z = _roll;
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.Euler(targetEulerRotation),
+            Time.deltaTime
+        );
     }
 }
---
+++ Assets/Game/SpaceshipSeat.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SpaceshipSeat : UdonSharpBehaviour
+{
+    public Spaceship spaceship;
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        if(player == Networking.LocalPlayer) spaceship.localPlayerUsing = true;
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        if(player == Networking.LocalPlayer) spaceship.localPlayerUsing = false;
+    }
+}
