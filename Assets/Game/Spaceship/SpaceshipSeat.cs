cacaad6e6a6e648b589f60792fcfe9c0b9ef2ab0
Hikari Luz <hikaridesuyoo@gmail.com>
2024-07-01 17:59:53 -0300

    feat: syncs spaceship

--- Assets/Game/Spaceship/SpaceshipSeat.cs
+++ Assets/Game/Spaceship/SpaceshipSeat.cs
@@ -9,7 +9,16 @@ public class SpaceshipSeat : UdonSharpBehaviour
     public Spaceship spaceship;
     public override void OnStationEntered(VRCPlayerApi player)
     {
-        if(player == Networking.LocalPlayer) spaceship.localPlayerUsing = true;
+        if(player == Networking.LocalPlayer)
+        {
+            spaceship.localPlayerUsing = true;
+            // this part is really important
+            // because then all the movement
+            // that is applied to the local
+            // player spaceship will be synced
+            // to everyboidy else
+            Networking.SetOwner(Networking.LocalPlayer, spaceship.gameObject);
+        }
     }
 
     public override void OnStationExited(VRCPlayerApi player)
