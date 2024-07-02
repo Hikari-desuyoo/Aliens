b596fa2c3aa4e3e594bfbbb190d9772f095ba475
Hikari Luz <hikaridesuyoo@gmail.com>
2024-07-02 02:42:06 -0300

    feat: spaceship model!

---
+++ Assets/Game/Spaceship/SeatButton.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SeatButton : UdonSharpBehaviour
+{
+    public VRCStation seat;
+
+    public override void Interact()
+    {
+        seat.UseStation(Networking.LocalPlayer);
+    }
+}
